3eedcb9096aeb96afe16424f18e46952658ea2b1
Matthias Komarek <matthias@mkomarek.de>
2018-07-09 15:30:16 +0200

    feat(package): remove global decorators from stories

--- generators/package/templates/_ComponentName/_ComponentName.story.js
+++ generators/package/templates/_ComponentName/_ComponentName.story.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withReadme } from 'storybook-readme';
-import { withKnobs } from '@storybook/addon-knobs/react';
 import { cosyMeta } from './package.json';
 
 import Readme from './README.md';
 
-import {withHtmlDecorator} from '../../addons/html-preview-addon';
 <%_ if(useProjectSwitch){ _%>
 import withProjectSwitchDecorator from '../../addons/project-switch-addon';
 <%_ } _%>
 import <%= componentName %> from './index';
 
 const stories = storiesOf(`${cosyMeta.type}|<%= componentName %>`, module)
-  .addDecorator(withReadme(Readme))
-  .addDecorator(withKnobs)
   <%_ if(useProjectSwitch){ _%>
-  .addDecorator(withHtmlDecorator)
+  .addDecorator(withReadme(Readme))
   .addDecorator(withProjectSwitchDecorator({projects: cosyMeta.projects}));
   <%_ } else{ _%>
-  .addDecorator(withHtmlDecorator);
+  .addDecorator(withReadme(Readme));
   <%_ } _%>
 
 
